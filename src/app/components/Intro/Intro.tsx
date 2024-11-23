2d3b3fa1508a338fc360307f27cbda33a3abe5e4
shin-minhyuk <afs_style@naver.com>
2024-11-23 20:06:12 +0900

    💄 design: intro 배경 추가, wave 색상 변경, 옵저버 패키지 추가

--- src/app/components/Intro/Intro.tsx
+++ src/app/components/Intro/Intro.tsx
@@ -1,11 +1,13 @@
 'use client';
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 export default function Intro() {
   const [currentText, setCurrentText] = useState<string[]>([]); // 현재 타이핑된 글자 배열
   const [lineIndex, setLineIndex] = useState(0); // 현재 타이핑 중인 줄
   const [startTyping, setStartTyping] = useState(false); // 타이핑 시작 여부
+  const [showMouseComponent, setShowMouseComponent] = useState(false); // 마우스 컴포넌트 표시 여부
+
   const lines = [
     { text: '안녕하세요', color: '#fff9f9' },
     { text: '새로운 도전에 성장하는', color: '#ecb921' },
@@ -16,11 +18,18 @@ export default function Intro() {
   const lineDelay = 100; // 한 줄 타이핑 완료 후 대기 시간 (ms)
 
   useEffect(() => {
-    const timer = setTimeout(() => {
+    const startTypingTimer = setTimeout(() => {
       setStartTyping(true);
     }, 4000);
 
-    return () => clearTimeout(timer); // 컴포넌트 언마운트 시 타이머 정리
+    const mouseShowTimer = setTimeout(() => {
+      setShowMouseComponent(true);
+    }, 9500);
+
+    return () => {
+      clearTimeout(startTypingTimer);
+      clearTimeout(mouseShowTimer);
+    };
   }, []);
 
   useEffect(() => {
@@ -47,7 +56,12 @@ export default function Intro() {
   }, [lineIndex, startTyping]); // 줄 변경 시 재실행
 
   return (
-    <section className='relative w-auto overflow-hidden' style={{ height: 'calc(100dvh + 173px)' }}>
+    <section
+      className='relative w-auto overflow-hidden'
+      style={{
+        height: 'calc(100dvh + 173px)',
+      }}
+    >
       {/* snow */}
       <div
         className='custom-snow absolute -top-[900px] left-0 right-0 z-20 block w-full'
@@ -55,8 +69,14 @@ export default function Intro() {
       />
       {/* title */}
       <div
-        className='flex flex-col items-center justify-center bg-[#373737] py-[72px]'
-        style={{ height: 'calc(100dvh)' }}
+        className='relative flex flex-col items-center justify-center bg-[#373737] py-[72px]'
+        style={{
+          height: 'calc(100dvh)',
+          backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url("/imgs/intro_background.jpg")',
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center',
+        }}
       >
         <h2 className='z-20 mx-auto mb-[36px] flex flex-col gap-2 text-center font-aggro text-5xl font-bold text-[#fff9f9]'>
           {lines.slice(0, lineIndex).map((line, idx) => (
@@ -71,10 +91,15 @@ export default function Intro() {
           </p>
         </h2>
         <div className='custom-bar relative h-2 w-0 max-w-[1200px] bg-[#fff9f9]' />
-        <div className='before:animate-scroll-indicator relative mt-[100px] h-[54px] w-[34px] rounded-[20px] border-2 border-[#fff9f9] before:absolute before:left-[13px] before:top-[10px] before:h-3 before:w-1 before:rounded-full before:bg-[#fff9f9] before:content-[""]' />
+        <div
+          className={`relative mt-[100px] h-[54px] w-[34px] rounded-[20px] border-2 border-[#fff9f9] opacity-0 transition-opacity duration-1000 ease-in-out before:absolute before:left-[13px] before:top-[10px] before:h-3 before:w-1 before:animate-scroll-indicator before:rounded-full before:bg-[#fff9f9] before:content-[""] ${
+            showMouseComponent ? 'opacity-100' : 'opacity-0'
+          }`}
+        />
+        <div className='absolute bottom-0 h-[100px] w-full bg-gradient-to-t from-[#000000B3] to-transparent'></div>
       </div>
       {/* slide */}
-      <div className='relative h-[173px] w-full overflow-x-hidden bg-white'>
+      <div className='relative h-[173px] w-full overflow-x-hidden'>
         {[
           { top: '-20px', left: '0', opacity: 1 }, // 첫 번째 웨이브
           { top: '-10px', left: '-100px', opacity: 0.5 }, // 두 번째 웨이브
@@ -82,7 +107,7 @@ export default function Intro() {
         ].map((style, i) => (
           <div
             key={i}
-            className='IntroWave absolute h-[173px] w-[6400px] bg-white'
+            className='IntroWave absolute h-[173px] w-[6400px]'
             style={{
               backgroundImage: `url(/imgs/wave.svg)`,
               backgroundRepeat: 'repeat-x',
